a2b7bfe79bc00049c38964d29ced80a84c3f42eb
kimyunsu <kimdia2000@gmail.com>
2021-06-09 22:25:07 +0900

    [spring Security] 로그인 정보 세션보관기능

--- spring_workspace/hello-spring-security/sql/security.sql
+++ spring_workspace/hello-spring-security/sql/security.sql
@@ -31,3 +31,21 @@ select * from member where id = 'admin';
 
 --회원권한 조회
 select * from authority where id = 'admin';
+
+--security의 remember-me 사용을 위한 persistent_logins 테이블 생성
+--반드시 아래와 같이 구조를 잡아야 security에서 인식함
+create table persistent_logins(
+    username varchar2(64) not null,
+    series varchar2(64) primary key,
+    token varchar2(64) not null, --username, password, expiry time에 대한 hashing값
+    last_used timestamp not null
+);
+
+select * from persistent_logins;
+
+
+
+
+
+
+
---
+++ spring_workspace/hello-spring-security/src/main/webapp/WEB-INF/views/error/accessDenied.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>오류</title>
+</head>
+<body>
+	<h1>접근권한이 없는 페이지입니다</h1>
+	<a href="${pageContext.request.contextPath }">홈으로</a>
+</body>
+</html>
